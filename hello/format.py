8fe261ca174cfa42fb12ae968ffa2cc9498ab6a7
Richard Lee <richard_875@me.com>
2022-10-13 21:59:52 +1100

    - Improve format script

--- hello/format.py
+++ hello/format.py
@@ -1,12 +1,13 @@
-def format(rest_result):
-    test_functions = ["Bitonic sort", "Brick Sort", "Bubble Sort", "Bucket Sort", "Cocktail Sort", "Comb Sort", "Gnome Sort", "Heap Sort", "Insertion Sort", "Merge Sort", "Pancake Sort", "Pigeonhole Sort", "Quick Sort", "Radix Sort", "Selection Sort", "Shell Sort", "Smooth Sort", "Strand Sort"]
+def format(rest_result, total_time):
     string_builder = ""
     string_builder += "<h2>Experiment 1: Benchmark</h2>"
 
     for key in rest_result:
         string_builder += f"<h2>{key}:</h2>"
-
-        for i in range(len(rest_result[key])):
-            string_builder += f"<h4 style='margin: 0;'>{test_functions[i]}: {rest_result[key][i]}s</h4>"
-
+        string_builder += f"<h4>Input small: {rest_result[key][0]}s</h4>"
+        string_builder += f"<h4>Input medium: {rest_result[key][1]}s</h4>"
+        string_builder += f"<h4>Input large: {rest_result[key][2]}s</h4>"
+        string_builder += "</br>"
+    
+    string_builder += f"<h2>Total time: {total_time}s</h2>"
     return string_builder
