16e65d62cc54688c35597a94d47aa61b8cc04c0d
Richard Lee <richard_875@me.com>
2022-10-13 13:10:00 +1100

    - Edit welcome script

--- hello/format.py
+++ hello/format.py
@@ -1,6 +1,7 @@
 def format(rest_result):
     test_functions = ["Bitonic sort", "Brick Sort", "Bubble Sort", "Bucket Sort", "Cocktail Sort", "Comb Sort", "Gnome Sort", "Heap Sort", "Insertion Sort", "Merge Sort", "Pancake Sort", "Pigeonhole Sort", "Quick Sort", "Radix Sort", "Selection Sort", "Shell Sort", "Smooth Sort", "Strand Sort"]
     string_builder = ""
+    string_builder += "<h2>Experiment 1: Benchmark</h2>"
 
     for key in rest_result:
         string_builder += f"<h2>{key}:</h2>"
